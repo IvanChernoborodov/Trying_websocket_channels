dbd57155ec5fc9a4d4daa361376a0e609ca9c029
trbshoot <skich1984>
2019-12-12 17:37:27 +0300

    working security

--- project/chat/consumers.py
+++ project/chat/consumers.py
@@ -1,58 +1,100 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-def it():
-    i = 0
-    while True:
-        yield i
-        i += 1
+class EchoConsumer(AsyncWebsocketConsumer):
+    # groups = ["broadcast"]
 
-x = it()
-async def prod():
-    async for i in x:
-        return i
-
-
-class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        print(self.room_name)
-        self.room_group_name = 'chat_%s' % self.room_name
-        print('connected')
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        # Called on connection.
+        # To accept the connection call:
         await self.accept()
 
+        # Or accept the connection and specify a chosen subprotocol.
+        # A list of subprotocols specified by the connecting client
+        # will be available in self.scope['subprotocols']
+        # await self.accept("subprotocol")
+        # To reject the connection, call:
+        # await self.close()
+
+    async def receive(self, text_data=None, bytes_data=None):
+        # Called with either text_data or bytes_data for each frame
+        # You can call:
+        await self.send(text_data="Hello world!")
+
+        # Or, to send a binary frame:
+        # await self.send(bytes_data="Hello world!")
+
+        # Want to force-close the connection? Call:
+        # await self.close()
+        # Or add a custom WebSocket error code!
+        # await self.close(code=4123)
+
     async def disconnect(self, close_code):
-        # Leave room group
-        print('disconected')
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        print('closed')
+        # Called when the socket closes
+        await self.close()
+
+
+
+
+
+
+
+
+
+# def it():
+#     i = 0
+#     while True:
+#         yield i
+#         i += 1
+#
+# x = it()
+# async def prod():
+#     async for i in x:
+#         return i
+
+
+# class ChatConsumer(AsyncWebsocketConsumer):
+    # async def connect(self):
+    #     # print(dir(self.scope))
+    #     # self.room_name = self.scope['url_route']['kwargs']['room_name']
+    #     # print(self.room_name)
+    #     # self.room_group_name = 'chat_%s' % self.room_name
+    #     # Join room group
+    #     # await self.channel_layer.group_add(
+    #     #     self.room_group_name,
+    #     #     self.channel_name
+    #     # )
+    #     print('connected')
+    #     await self.accept()
+
+    # async def disconnect(self, close_code):
+    #     # Leave room group
+    #     print('disconected')
+    #     await self.channel_layer.group_discard(
+    #         self.room_group_name,
+    #         self.channel_name
+    #     )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+    # async def receive(self, text_data):
+    #     text_data_json = json.loads(text_data)
+    #     message = text_data_json['message']
+    #
+    #     # Send message to room group
+    #     await self.channel_layer.group_send(
+    #         self.room_group_name,
+    #         {
+    #             'type': 'chat_message',
+    #             'message': message
+    #         }
+    #     )
 
     # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
-        print(message)
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': prod()
-        }))
+    # async def chat_message(self, event):
+    #     message = event['message']
+    #     print(message)
+    #     # Send message to WebSocket
+    #     await self.send(text_data=json.dumps({
+    #         'message': message,
+    #     }))
--- project/project/routing.py
+++ project/project/routing.py
@@ -2,12 +2,24 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+from channels.security.websocket import OriginValidator
+
+
+# application = ProtocolTypeRouter({
+#     # (http->django views is added by default)
+#     'websocket': AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         ),
+#     ),
+#
+# })
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+    "websocket": OriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns),
+        ),
+        ["http://127.0.0.1:8000"],
     ),
 })
